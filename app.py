db6a9f571fa447ed5c4138d8923712cec569d4cd
Denis Petri <denis_petri@outlook.com>
2022-05-30 20:00:29 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Desafio 1 Concluido"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
 if __name__ == '__main__':
     app.run()
